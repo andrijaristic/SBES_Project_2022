23cffb680e5630f8892b46b754aa5314bc69ce12
Filip Ilic <filipflyer98@gmail.com>
2022-12-27 18:56:56 +0100

    Added utilities for working with certificates

---
+++ Project/Utilities/CertificateManager/ClientCertValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.CertificateManager
+{
+    public class ClientCertValidator : X509CertificateValidator
+    {
+        string companyName = "CN=TestCA";
+        public override void Validate(X509Certificate2 certificate)
+        {
+            if (!certificate.Issuer.Equals(companyName))
+            {
+                throw new Exception("Certificate is self-issued.");
+            }
+        }
+    }
+}
---
+++ Project/Utilities/CertificateManager/ServiceCertValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.CertificateManager
+{
+    public class ServiceCertValidator : X509CertificateValidator
+    {
+        string companyName = "TestCA";
+        public override void Validate(X509Certificate2 certificate)
+        {
+            // CN=wcfadmin,OU=Readers_Writers_Admins,O=TestCA
+            string companyName;
+            try
+            {
+                companyName = certificate.Subject.Split(',')[2].Split('=')[1];
+            }
+            catch
+            {
+                throw new Exception("Certificate is not from the valid issuer.");
+            }
+
+            if (!this.companyName.Equals(companyName))
+            {
+                throw new Exception("Certificate is not from the valid issuer.");
+            }
+        }
+    }
+}
