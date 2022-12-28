2d42d1403d706c47888263f9e2488a7967683dbd
Filip Ilic <filipflyer98@gmail.com>
2022-12-28 17:31:15 +0100

    Changed Consumer implementation

--- Project/Contracts/Consumer.cs
+++ Project/Contracts/Consumer.cs
@@ -14,15 +14,16 @@ namespace Contracts
         private string region;
         private string city;
         private int year;
-        private double amount;
+        private double[] amounts;   // 12 vrednosti, po jedna za svaki mesec
 
-        public Consumer(string region, string city, int year, double amount)
+        public Consumer(string region, string city, int year)
         {
             this.Id = Guid.NewGuid();
             this.Region = region;
             this.City = city;
             this.Year = year;
-            this.Amount = amount;
+            amounts = new double[12];
+            
         }
 
         [DataMember]
@@ -34,7 +35,7 @@ namespace Contracts
         [DataMember]
         public int Year { get => year; set => year = value; }
         [DataMember]
-        public double Amount { get => amount; set => amount = value; }
+        public double[] Amounts { get => amounts; set => amounts = value; }
 
 
         public static string ToString(Consumer consumer)
@@ -43,7 +44,13 @@ namespace Contracts
             {
                 return "";
             }
-            return consumer.Id + ";" + consumer.Region + ";" + consumer.City + ";" + consumer.Year + ";" + consumer.Amount;
+            string consumerString = consumer.Id + ";" + consumer.Region + ";" + consumer.City + ";" + consumer.Year + ";";
+            for(int i = 0; i < 12; i++)
+            {
+                consumerString += consumer.Amounts[i] + ";";
+            }
+            consumerString = consumerString.Remove(consumerString.Length - 1, 1);
+            return consumerString;
         }
 
         public static Consumer FromString(string consumerString)
@@ -56,8 +63,14 @@ namespace Contracts
                 string city = properties[2];
                 int year = int.Parse(properties[3]);
                 double amount = double.Parse(properties[4]);
-                Consumer consumer = new Consumer(region, city, year, amount);
+                double[] amounts = new double[12];
+                for(int i = 4; i < 15; i++)
+                {
+                    amounts[i-4] = double.Parse(properties[i]);
+                }
+                Consumer consumer = new Consumer(region, city, year);
                 consumer.id = id;
+                consumer.Amounts = amounts;
                 return consumer;
             }
             catch
