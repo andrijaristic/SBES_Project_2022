fd6d13cb932d0adc9edccdcd5dfe9739127d982b
Filip Ilic <filipflyer98@gmail.com>
2022-12-28 17:29:31 +0100

    Added months enumeration

---
+++ Project/Contracts/Months.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    [DataContract]
+    public enum Months 
+    {
+        [EnumMember] JANUAR, 
+        [EnumMember] FEBRUAR, 
+        [EnumMember] MART, 
+        [EnumMember] APRIL, 
+        [EnumMember] MAJ,
+        [EnumMember] JUN,
+        [EnumMember] JUL,
+        [EnumMember] AVGUST,
+        [EnumMember] SEPTEMBAR,
+        [EnumMember] OKTOBAR,
+        [EnumMember] NOVEMBAR,
+        [EnumMember] DECEMBAR
+    }
+}
